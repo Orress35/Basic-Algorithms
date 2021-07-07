7315f67f2f6fc61924cbff337208c25b7bf7a795
Orress35 <79585248+Orress35@users.noreply.github.com>
2021-07-07 21:27:49 +0300

    Create divisors.c

---
+++ c/divisors.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int getDivisors(long long n) {
+    if (n <= 1) return 1;
+    int divisors = 1;
+    if (n % 2 != 0) {
+        for (long long i = 1; i <= n/2; i += 2)
+            if (n % i == 0) divisors++;
+    } else {
+        for (long long i = 1; i <= n/2; i++)
+            if (n % i == 0) divisors++;
+    }
+    return divisors;
+}
+
+// driver code
+int main() {
+    printf("divisors: %d\n", getDivisors(4324320));
+    return 0;
+}
