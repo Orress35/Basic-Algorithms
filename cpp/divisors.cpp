573238aad11ff0d435d880b84ce37e1acca066b6
Orress35 <79585248+Orress35@users.noreply.github.com>
2021-07-07 21:28:03 +0300

    Create divisors.cpp

---
+++ cpp/divisors.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+int getDivisors(long long n) {
+    if (n <= 1) return 1;
+    int divisors = 1;
+    if (n % 2 != 0) {
+        for (long long i = 1; i <= n/2; i += 2)
+            if (n % i == 0) divisors++;
+    } else {
+        for (long long i = 1; i <= n/2; i++)
+            if (n % i == 0) divisors++;
+    }
+    return divisors;
+}
+
+// driver code
+int main() {
+    std::cout << "divisors: " << getDivisors(4324320) << std::endl;
+    exit(0);
+}
