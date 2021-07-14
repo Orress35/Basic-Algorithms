ff3713ae7f7908bfc3d32f0a3cd112d47a2b4fd1
Orress35 <79585248+Orress35@users.noreply.github.com>
2021-07-14 19:14:23 +0300

    Add files via upload

---
+++ javascript/primes.js
@@ -0,0 +1,11 @@
+function isprime(n) {
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (let i = 5; i < Math.sqrt(n); i += 6) if (n % i == 0 || n % (i+2) == 0) return false;
+    return true;
+}
+
+// driver code
+for (let i = 1; i < 1000; i++)
+    if (isprime(i))
+        console.log("prime number found: " + i);
