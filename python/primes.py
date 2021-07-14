b5a9f28d5c8912729645938e5bfa5998ac94b953
Orress35 <79585248+Orress35@users.noreply.github.com>
2021-07-14 19:14:06 +0300

    Add files via upload

---
+++ python/primes.py
@@ -0,0 +1,15 @@
+import math
+
+def isprime(n):
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return True
+    for i in range(5, math.sqrt(n)+1, 6):
+        if n % i == 0:
+            return True
+    return False
+
+for i in range(1, 1000):
+    if isprime(i):
+        print("prime number found: %d", i)
