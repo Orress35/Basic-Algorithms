239b36054c554d58058adb13c3391a238efec4aa
Orress <orress35@mail.ru>
2021-07-17 17:02:05 +0300

    Added catalan.py

---
+++ python/catalan.py
@@ -0,0 +1,16 @@
+def catalan(n):
+    if n <= 0:
+        return 1
+
+    catalan = [0] * (n+1)
+    catalan[0] = catalan[1] = 1
+
+    for i in range(2, n+1):
+        for j in range(i):
+            catalan[i] += catalan[j] * catalan[i-j-1]
+
+    return catalan[n]
+ 
+# driver code
+for i in range(25):
+    print(catalan(i))
