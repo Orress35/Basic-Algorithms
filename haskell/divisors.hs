3fe7c98af958d0703c74e76f03bfda2a25e4394d
Orress <orress35@mail.ru>
2021-07-18 15:29:45 +0300

    optimized divisors.hs from 195 ms to 2 ms

--- haskell/divisors.hs
+++ haskell/divisors.hs
@@ -1,4 +1,8 @@
-divisors n = 1 : n : filter ((0 ==) . (n `mod`)) [2 .. (n `div` 2)]
+isqrt n = floor(sqrt(fromIntegral n))
+
+divisors :: Int -> Int
+divisors 1 = 1
+divisors n = (length [x | x <- [1..isqrt n], n `rem` x == 0]) * 2
 
 main = do
     print(divisors 4324320)
